ad6180f44cc9ad2e789c1aad0a37107cd8541555
Toan Nguyen <nta.toan@gmail.com>
2021-01-27 21:43:18 -0500

    Update Readme

--- packages/server/database/factories/index.ts
+++ packages/server/database/factories/index.ts
@@ -21,7 +21,7 @@ const incomeCategories = ['savings', 'paycheck', 'bonus', 'interest', 'other']
 
 export const ExpensesFactory = Factory.define(Expense, ({ faker }) => {
   const ex = new Expense()
-  ex.date = DateTime.fromJSDate(faker.date.between('2021-01-01', '2020-01-01'))
+  ex.date = DateTime.fromJSDate(faker.date.between('2021-01-01', '2020-06-01'))
   ex.amount = parseFloat(faker.finance.amount(1, 1000, 2))
   ex.description = faker.lorem.sentence()
   ex.category = expensesCategories[faker.random.number(expensesCategories.length - 1)]
@@ -31,7 +31,7 @@ export const ExpensesFactory = Factory.define(Expense, ({ faker }) => {
 
 export const IncomesFactory = Factory.define(Income, ({ faker }) => {
   const ex = new Income()
-  ex.date = DateTime.fromJSDate(faker.date.between('2021-02-01', '2020-11-11'))
+  ex.date = DateTime.fromJSDate(faker.date.between('2021-01-01', '2020-06-01'))
   ex.amount = parseFloat(faker.finance.amount(1, 1000, 2))
   ex.description = faker.lorem.sentence()
   ex.category = incomeCategories[faker.random.number(incomeCategories.length - 1)]
