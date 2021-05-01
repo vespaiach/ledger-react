bc150cdee7f00c61ad8f8dfa34565d0c4517bd60
Toan Nguyen <nta.toan@gmail.com>
2021-05-01 08:30:08 -0400

    CRUD category

--- packages/server/src/route/category.ts
+++ packages/server/src/route/category.ts
@@ -3,6 +3,7 @@ import HttpStatusCode, {
   CreateCategoryRequest,
   DeleteCategoryRequest,
   QueryCategoriesRequest,
+  UpdateCategoryRequest,
 } from '../types';
 import { CustomError } from '../util';
 
@@ -11,6 +12,8 @@ export const checkCreateCategory: CreateCategoryRequest = async (req, res, next)
 
   if (!req.body.name) {
     errors['name'] = '"name" is required, but missing';
+  } else if (req.body.name.length > 255) {
+    errors['name'] = '"name" is too long, limit to 255 characters only';
   }
 
   if (!req.body.transactionType) {
@@ -24,6 +27,8 @@ export const checkCreateCategory: CreateCategoryRequest = async (req, res, next)
   if (Object.keys(errors).length > 0) {
     next(new CustomError(HttpStatusCode.BAD_REQUEST, { fields: errors }));
   } else {
+    req.body.name = String(req.body.name);
+
     next();
   }
 };
@@ -46,15 +51,15 @@ export const queryCategories: QueryCategoriesRequest = async (_, res, next) => {
   try {
     const prisma = res.locals.prisma;
     res.status(200).send({
-      data: await prisma.category.findMany(),
+      data: await prisma.category.findMany({ orderBy: [{ createdAt: 'desc' }] }),
     });
   } catch (err) {
     next(err);
   }
 };
 
-export const checkDeleteCategories: DeleteCategoryRequest = async (req, res, next) => {
-  if (!req.query.id || req.query.id <= 0) {
+export const checkDeleteCategory: DeleteCategoryRequest = async (req, res, next) => {
+  if (!req.query.id || isNaN(req.query.id) || req.query.id <= 0) {
     next(
       new CustomError(HttpStatusCode.BAD_REQUEST, {
         fields: {
@@ -63,11 +68,30 @@ export const checkDeleteCategories: DeleteCategoryRequest = async (req, res, nex
       })
     );
   } else {
-    next();
+    try {
+      const counter = await res.locals.prisma.transaction.count({
+        where: {
+          categoryId: Number(req.query.id),
+        },
+      });
+
+      if (counter > 0) {
+        next(
+          new CustomError(HttpStatusCode.LOCKED, {
+            message: "Couldn't delete this category. It is being referencing by others",
+          })
+        );
+      } else {
+        req.query.id = Number(req.query.id);
+        next();
+      }
+    } catch (err) {
+      next(err);
+    }
   }
 };
 
-export const deleteCategories: DeleteCategoryRequest = async (req, res, next) => {
+export const deleteCategory: DeleteCategoryRequest = async (req, res, next) => {
   try {
     await res.locals.prisma.category.delete({
       where: {
@@ -76,6 +100,60 @@ export const deleteCategories: DeleteCategoryRequest = async (req, res, next) =>
     });
 
     res.status(200).send({});
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const checkUpdateCategory: UpdateCategoryRequest = async (req, res, next) => {
+  const errors: Record<string, string> = {};
+
+  if (!req.body.id || isNaN(req.body.id) || req.body.id <= 0) {
+    errors['id'] = '"id" is required, but missing';
+  }
+
+  if (req.body.name && String(req.body.name).length > 255) {
+    errors['name'] = '"name" is too long, limit to 255 characters only';
+  }
+
+  if (
+    req.body.transactionType &&
+    req.body.transactionType !== TransactionType.EXPENSE &&
+    TransactionType.INCOME
+  ) {
+    errors['transactionType'] = '"transactionType" is either "EXPENSE" or "INCOME"';
+  }
+
+  if (Object.keys(errors).length > 0) {
+    next(new CustomError(HttpStatusCode.BAD_REQUEST, { fields: errors }));
+  } else {
+    req.body.id = Number(req.body.id);
+    req.body.name = String(req.body.name);
+
+    next();
+  }
+};
+
+export const updateCategory: UpdateCategoryRequest = async (req, res, next) => {
+  try {
+    const data: { name?: string; transactionType?: TransactionType } = {};
+    if (req.body.name) {
+      data.name = req.body.name;
+    }
+    if (req.body.transactionType) {
+      data.transactionType = req.body.transactionType;
+    }
+
+    const category = await res.locals.prisma.category.update({
+      where: {
+        id: req.body.id,
+      },
+      data,
+    });
+
+    res.status(200).send({
+      data: category,
+    });
   } catch (err) {
     next(err);
   }
--- packages/server/src/route/index.ts
+++ packages/server/src/route/index.ts
@@ -2,10 +2,21 @@ import { Express } from 'express';
 import HttpStatusCode from '../types';
 import { CustomError } from '../util';
 
-import { checkCreateCategory, createCategory } from './category';
+import {
+  checkCreateCategory,
+  checkDeleteCategory,
+  checkUpdateCategory,
+  createCategory,
+  deleteCategory,
+  queryCategories,
+  updateCategory,
+} from './category';
 
 export default function createRouter(app: Express) {
   app.post('/api/categories', checkCreateCategory, createCategory);
+  app.get('/api/categories', queryCategories);
+  app.put('/api/categories', checkUpdateCategory, updateCategory);
+  app.delete('/api/categories', checkDeleteCategory, deleteCategory);
 
   app.use(function (_, __, next) {
     next(new CustomError(HttpStatusCode.NOT_FOUND, { message: 'route did not exist' }));
