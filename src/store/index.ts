ea7ced41d7ea3ef49d8c185bde9a3cfa65855a04
Toan Nguyen <nta.toan@gmail.com>
2021-09-11 21:50:03 -0400

    feat: config saga

---
+++ src/store/index.ts
@@ -0,0 +1,27 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import saga from './saga';
+import localStore from './state';
+import repo from './repo';
+
+const sagaMiddleware = createSagaMiddleware({
+  onError: (e) => {
+    console.error(e);
+  },
+});
+const typedWindow = typeof window !== 'undefined' && (window as any);
+const composeEnhancers =
+  (process.env.NODE_ENV === 'development' &&
+    typeof typedWindow !== 'undefined' &&
+    typedWindow.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+    typedWindow.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      trace: true,
+      traceLimit: 25,
+    })) ||
+  compose;
+
+const store = createStore(localStore, composeEnhancers(applyMiddleware(sagaMiddleware)));
+sagaMiddleware.run(saga(repo));
+
+export { store };
