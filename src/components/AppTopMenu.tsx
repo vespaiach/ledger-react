901ebc2743006abaaa15fd79002780dbb83486f4
Toan Nguyen <nta.toan@gmail.com>
2021-10-09 12:00:13 -0400

    chore: dont hide top bar menu when scrolling

--- src/components/AppTopMenu.tsx
+++ src/components/AppTopMenu.tsx
@@ -6,7 +6,6 @@ import {
   BarChartRounded as BarChartIcon,
 } from '@mui/icons-material';
 
-import { HideOnScroll } from './HideOnScroll';
 import { AppCommand, CommandFunc } from '../types';
 import { useResponsive } from '../hooks/useResponsive';
 
@@ -22,49 +21,43 @@ export function AppTopMenu({ onCommand }: MasterProps) {
   };
 
   return (
-    <HideOnScroll>
-      <AppBar sx={{ backgroundColor: 'background.paper' }}>
-        <Container maxWidth="md" disableGutters>
-          <Toolbar disableGutters>
-            <IconButton
-              size="large"
-              edge={iconEdge}
-              color="primary"
-              aria-label="Add transaction"
-              onClick={handleClick(AppCommand.AddTransaction)}
-            >
-              <AddIcon />
-            </IconButton>
-            <IconButton
-              edge={iconEdge}
-              size="large"
-              color="primary"
-              aria-label="Search transaction"
-              onClick={handleClick(AppCommand.SearchTransaction)}
-            >
-              <SearchIcon />
-            </IconButton>
-            <IconButton
-              edge={iconEdge}
-              size="large"
-              color="primary"
-              aria-label="Open chart"
-              onClick={handleClick(AppCommand.OpenChart)}
-            >
-              <BarChartIcon />
-            </IconButton>
-            <IconButton
-              size="large"
-              color="primary"
-              edge={iconEdge}
-              aria-label="Open side navigation"
-              onClick={handleClick(AppCommand.OpenSideNavigation)}
-            >
-              <MoreVertIcon />
-            </IconButton>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </HideOnScroll>
+    <AppBar sx={{ backgroundColor: 'background.paper' }}>
+      <Container maxWidth="md" disableGutters>
+        <Toolbar disableGutters>
+          <IconButton
+            size="large"
+            edge={iconEdge}
+            color="primary"
+            aria-label="Add transaction"
+            onClick={handleClick(AppCommand.AddTransaction)}>
+            <AddIcon />
+          </IconButton>
+          <IconButton
+            edge={iconEdge}
+            size="large"
+            color="primary"
+            aria-label="Search transaction"
+            onClick={handleClick(AppCommand.SearchTransaction)}>
+            <SearchIcon />
+          </IconButton>
+          <IconButton
+            edge={iconEdge}
+            size="large"
+            color="primary"
+            aria-label="Open chart"
+            onClick={handleClick(AppCommand.OpenChart)}>
+            <BarChartIcon />
+          </IconButton>
+          <IconButton
+            size="large"
+            color="primary"
+            edge={iconEdge}
+            aria-label="Open side navigation"
+            onClick={handleClick(AppCommand.OpenSideNavigation)}>
+            <MoreVertIcon />
+          </IconButton>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
